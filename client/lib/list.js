7f5e9dfa18f36afe073971a5146e34e192e658a6
kevin <faithful.7up@gmail.com>
2016-02-02 08:39:13 +0700

    finish user authentication

---
+++ client/lib/list.js
@@ -0,0 +1,9 @@
+List = {
+    categories(){
+        let list = [{label: 'ប្រលោមលោក', value: 'novel'}, {
+            label: 'រឿងខ្មោច',
+            value: 'ghost'
+        }, {label: 'កំប្លែង', value: 'comedy'}];
+        return list;
+    }
+};
