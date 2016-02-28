b894f6d1a8f35d9ec17f00e6b73bd941f2034336
phirum <phirumsim@gmail.com>
2016-02-28 17:54:57 +0700

    update exchange rate and invoice

--- client/template/invoice/invoice.js
+++ client/template/invoice/invoice.js
@@ -1,8 +1,14 @@
 Template.restaurantInvoice.helpers({
-  data() {
-    let paymentId = Router.current().params.paymentId;
-    Fetcher.setDefault(paymentId, false);
-    Fetcher.retrieve(paymentId, 'findPaymentInvoice', paymentId);
-    return Fetcher.get(paymentId);
-  }
+    data() {
+        let paymentId = Router.current().params.paymentId;
+        Fetcher.setDefault(paymentId, false);
+        Fetcher.retrieve(paymentId, 'findPaymentInvoice', paymentId);
+        return Fetcher.get(paymentId);
+    },
+    goBack(){
+        Meteor.setTimeout(()=> {
+            window.print();
+            Router.go('/restaurant/sale');
+        }, 1000)
+    }
 });
