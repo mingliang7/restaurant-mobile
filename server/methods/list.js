a1c10485da2b704bc36edc32592fc536e22afe25
kevin <faithful.7up@gmail.com>
2016-03-06 10:05:14 +0700

    sort by name in report

--- server/methods/list.js
+++ server/methods/list.js
@@ -1,32 +1,36 @@
 Meteor.methods({
-    getCustomerList(){
-        var list = [];
-        Restaurant.Collection.Customers.find().forEach(function (obj) {
-            list.push({
-                label: obj.name,
-                value: obj._id
-            });
-        });
-        return list;
-    },
-    getCategoryList(){
-        var list = [];
-        Restaurant.Collection.Categories.find().forEach(function (obj) {
-            list.push({
-                label: obj.name,
-                value: obj._id
-            });
-        });
-        return list;
-    },
-    getUserList(){
-        var list = [];
-        Meteor.users.find().forEach((obj)=> {
-            list.push({
-                label:obj.profile.username,
-                value:obj._id
-            })
-        });
-        return list;
-    }
+  getCustomerList() {
+    var list = [];
+    Restaurant.Collection.Customers.find().forEach(function(obj) {
+      list.push({
+        label: obj.name,
+        value: obj._id
+      });
+    });
+    return list;
+  },
+  getCategoryList() {
+    var list = [];
+    Restaurant.Collection.Categories.find({}, {
+      sort: {
+        name: 1
+      }
+    }).forEach(function(obj) {
+      list.push({
+        label: obj.name,
+        value: obj._id
+      });
+    });
+    return list;
+  },
+  getUserList() {
+    var list = [];
+    Meteor.users.find().forEach((obj) => {
+      list.push({
+        label: obj.profile.username,
+        value: obj._id
+      })
+    });
+    return list;
+  }
 });
