593ec8dff4ec28133244321bd165565bb02d9834
kevin <imac@Is-iMac-2.local>
2016-03-18 10:01:17 +0700

    add sale detail report with product show

--- both/router/reports/saleDetail.js
+++ both/router/reports/saleDetail.js
@@ -1,17 +1,30 @@
 Router.map(function() {
-    this.route('restaurant.sale.detail.report', {
-        path: '/restaurant/sale-detail-report',
-        onBeforeAction: function(pause) {
-            if (!Meteor.user()) {
-                // render the login template but keep the url in the browser the same
-                Router.go('/')
-            }
-            Restaurant.Roles.checkRoles(Meteor.userId(), ['setting', 'cashier']);
-            this.next();
-        }
-    });
-    this.route('restaurant.sale.detail.report.gen', {
-        path: '/restaurant/sale-detail-report-gen',
-        layoutTemplate: 'reportLayout'
-    });
+  this.route('restaurant.sale.detail.report', {
+    path: '/restaurant/sale-detail-report',
+    onBeforeAction: function(pause) {
+      if (!Meteor.user()) {
+        // render the login template but keep the url in the browser the same
+        Router.go('/');
+      }
+      Restaurant.Roles.checkRoles(Meteor.userId(), ['setting', 'cashier']);
+      this.next();
+    }
+  });
+  this.route('restaurant.sale.detail.report.gen', {
+    path: '/restaurant/sale-detail-report-gen',
+    layoutTemplate: 'reportLayout'
+  });
+  this.route('restaurant.sale.detail.product.report', {
+    path: '/restaurant/sale-detail-product-report',
+    onBeforeAction: function(pause) {
+      if (!Meteor.user()) {
+        Router.go('/');
+      }
+      this.next();
+    }
+  });
+  this.route('restaurant.sale.detail.product.report.gen',{
+    path: '/restaurant/sale-detail-product-report-gen',
+    layoutTemplate: 'reportLayout'
+  });
 });
