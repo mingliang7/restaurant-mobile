9e203a284ee1b1d4b72828eb0c096c1a889aaf21
kevin <imac@Is-iMac-2.local>
2016-03-18 11:28:09 +0700

    refactor outstanding report

--- client/template/reports/owedCustomerReport/outstandingReport.js
+++ client/template/reports/owedCustomerReport/outstandingReport.js
@@ -1,23 +1,24 @@
-Template.restaurantOutstandingReport.onRendered(function () {
-    $('[name="date"]').datetimepicker();
+Template.restaurantOutstandingReport.onRendered(function() {
+  $('[name="date"]').datetimepicker();
 });
 
 Template.restaurantOutstandingReport.helpers({
-    customers(){
-        return ReactiveMethod.call('getCustomerList');
-    }
+  customers() {
+    return ReactiveMethod.call('getCustomerList');
+  }
 });
 
 Template.restaurantOutstandingReportGen.helpers({
-    data: function () {
-        var query = Router.current().params.query;
-        var params = "getOutstandingReport";
-        Fetcher.setDefault(params, false);
-        Fetcher.retrieve(params, 'getOutstandingReport', query);
-        return Fetcher.get(params);
-    }
+  data: function() {
+    var query = Router.current().params.query;
+    var params = "getOutstandingReport";
+    Fetcher.setDefault(params, false);
+    Fetcher.retrieve(params, 'getOutstandingReport', query);
+    return Fetcher.get(params);
+  },
+  footer: function() {
+   var sales = this.content;
+   var totalInFooter = ReactiveMethod.call('extractContent', sales);
+   return totalInFooter;
+ }
 });
-
-
-
-
