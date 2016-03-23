de77c45b318cc7d34abcee334feca69d0e148408
kevin <faithful.7up@gmail.com>
2016-03-23 22:16:05 +0700

    sale detail report from pr in progress

--- both/collection/reports/saleDetailReport.js
+++ both/collection/reports/saleDetailReport.js
@@ -2,39 +2,59 @@
  * Schema
  */
 Restaurant.Schema.SaleDetailReport = new SimpleSchema({
-    categoryId: {
-        type: String,
-        label: "ផ្នែក",
-        autoform: {
-          firstOption: 'All'
-        },
-        optional:true
+  categoryId: {
+    type: String,
+    label: "ផ្នែក",
+    autoform: {
+      firstOption: 'All'
     },
-    customerId:{
-        type:String,
-        label:"អតិថិជន",
-        autoform: {
-          firstOption: 'All'
-        },
-        optional:true
-
-    },
-    fromDate: {
-        type: String,
-        label: "កាលបរិច្ឆេទចាប់ផ្ដើម",
-        optional:true
-    },
-    toDate:{
-        type:String,
-        label:"កាលបរិច្ឆេទបញ្ចប់",
-        optional:true
-    },
-    staffId:{
-      type:Date,
-      label:"បុគ្កលិក",
-      autoform: {
-        firstOption: 'All'
-      },
-      optional:true
+    optional: true
+  },
+  status: {
+    type: [String],
+    label: 'ប្រភេទវិក័យប័ត្រ',
+    optional: true,
+    autoform: {
+      type: 'select',
+      options() {
+        return [{
+          label: 'កំពុងលក់',
+          value: 'active'
+        }, {
+          label: 'ទូរទាត់រួច',
+          value: 'closed'
+        }, {
+          label: 'ផ្អាក',
+          value: 'canceled'
+        }];
+      }
     }
+  },
+  customerId: {
+    type: String,
+    label: "អតិថិជន",
+    autoform: {
+      firstOption: 'All'
+    },
+    optional: true
+
+  },
+  fromDate: {
+    type: String,
+    label: "កាលបរិច្ឆេទចាប់ផ្ដើម",
+    optional: true
+  },
+  toDate: {
+    type: String,
+    label: "កាលបរិច្ឆេទបញ្ចប់",
+    optional: true
+  },
+  staffId: {
+    type: Date,
+    label: "បុគ្កលិក",
+    autoform: {
+      firstOption: 'All'
+    },
+    optional: true
+  }
 });
--- client/template/reports/saleDetailReport/saleDetailReport.js
+++ client/template/reports/saleDetailReport/saleDetailReport.js
@@ -1,43 +1,64 @@
-Template.restaurantSaleDetailReport.onRendered(function () {
-    $('[name="fromDate"]').datetimepicker();
-    $('[name="toDate"]').datetimepicker();
+Template.restaurantSaleDetailReport.onRendered(function() {
+  $('[name="fromDate"]').datetimepicker();
+  $('[name="toDate"]').datetimepicker();
 });
 
 Template.restaurantSaleDetailReport.helpers({
-    customers()
-{
+  customers() {
     return ReactiveMethod.call('getCustomerList');
-}
-,
-categories()
-{
+  },
+  categories() {
     return ReactiveMethod.call('getCategoryList')
-}
-})
-;
+  }
+});
 
 Template.restaurantSaleDetailReportGen.helpers({
-    getNumberOfRow: function (transaction) {
-        return transaction.length;
-    },
-    options: function () {
-        // font size = null (default), bg
-        // paper = a4, a5, mini
-        // orientation = portrait, landscape
-        return {
-            //fontSize: 'bg',
-            paper: 'a4',
-            orientation: 'portrait'
-        };
-    },
-    data: function () {
-        // Get query params
-        //FlowRouter.watchPathChange();
-        var query = Router.current().params.query;
-        var params = "saleDetailReport";
-        Fetcher.setDefault(params, false);
-        Fetcher.retrieve(params, 'getSaleDetailReport', query);
-        return Fetcher.get(params);
-    }
+  getNumberOfRow: function(transaction) {
+    return transaction.length;
+  },
+  options: function() {
+    // font size = null (default), bg
+    // paper = a4, a5, mini
+    // orientation = portrait, landscape
+    return {
+      //fontSize: 'bg',
+      paper: 'a4',
+      orientation: 'portrait'
+    };
+  },
+  data: function() {
+    // Get query params
+    //FlowRouter.watchPathChange();
+    var query = Router.current().params.query;
+    var params = "saleDetailReport";
+    Fetcher.setDefault(params, false);
+    Fetcher.retrieve(params, 'getSaleDetailReport', query);
+    return Fetcher.get(params);
+  }
 
+});
+
+
+AutoForm.hooks({
+  restaurantSaleDetailReport: {
+    onSubmit(doc) {
+      this.event.preventDefault();
+      let status = '';
+      if (doc.status) {
+        doc.status = doc.status.join(',');
+        status = doc.status;
+      }
+      let fromDate = moment($('[name="fromDate"]').val()).format('YYYY-MM-DD HH:mm:ss');
+      let toDate =  moment($('[name="toDate"]').val()).format('YYYY-MM-DD HH:mm:ss');
+      let customerId = doc.customerId === undefined ? '' : doc.customerId;
+      let categoryId = doc.categoryId === undefined ? '' : doc.categoryId ;
+      let url = `/restaurant/sale-detail-report-gen?fromDate=${fromDate}&toDate=${toDate}&customerId=${customerId}&categoryId=${categoryId}&status=${status}`;
+      this.done(); //reset form
+      window.open(url, '_blank');
+
+    },
+    onSuccess(formType, result){
+
+    }
+  }
 });
