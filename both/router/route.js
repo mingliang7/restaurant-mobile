b29f7a77d8b791021f1f316de59c6ef1a8ecb2d8
kevin <faithful.7up@gmail.com>
2016-03-28 15:53:43 +0700

    update roles for route

--- both/router/route.js
+++ both/router/route.js
@@ -147,7 +147,7 @@ Router.map(function() {
         // render the login template but keep the url in the browser the same
         Router.go('/')
       }
-      Restaurant.Roles.checkRoles(Meteor.userId(), ['setting', 'cashier']);
+      Restaurant.Roles.checkRoles(Meteor.userId(), ['setting', 'super']);
       this.next();
     }
   });
@@ -477,7 +477,7 @@ Router.map(function() {
       if (!Meteor.userId()) {
         Router.go('/');
       }
-      Restaurant.Roles.checkRoles(Meteor.userId(), ['setting', 'super']);
+      Restaurant.Roles.checkRoles(Meteor.userId(), ['cashier','setting', 'super']);
       this.next();
     }
   });
