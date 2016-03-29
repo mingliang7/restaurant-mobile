878600b21d2d72f747c264ad9087f570d8b541f8
kevin <faithful.7up@gmail.com>
2016-03-29 16:15:25 +0700

    add rtb foods to private folder

--- server/methods/tasks/product.js
+++ server/methods/tasks/product.js
@@ -1,10 +1,21 @@
+/**
+Category:
+  001: អាហារពេលព្រឹក
+  002: ម្ហូបអាំងចៀនពង
+  003: សម្លរ
+  004:​ ញាំុ
+  005​: ម្ហូបឆា
+**/
+
+
 Meteor.methods({
   fetchProduct(assetFile) {
-    let products  = JSON.parse(Assets.getText(`products/${assetFile}`)) //fetch products from private/products file;
-    let index = 0;
+    let products  = JSON.parse(Assets.getText(`products/${assetFile}`)); //fetch products from private/products file;
+    let index = 1;
     if (products.length > 0) {
       products.forEach(function(product) {
-        Restaurant.Collection.Products.insert(product)
+        Restaurant.Collection.Products.insert(product);
+        console.log(index);
         index += 1;
       });
     }
