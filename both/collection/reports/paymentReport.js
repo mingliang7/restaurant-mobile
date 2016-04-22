190ae7b5557abf54b58fbeab71e3244f755ccd3c
yousour <hengyousour@gmail.com>
2016-04-22 12:03:40 +0700

    payment report riel

---
+++ both/collection/reports/paymentReport.js
@@ -0,0 +1,40 @@
+/**
+ * Schema
+ */
+Restaurant.Schema.PaymentReport = new SimpleSchema({
+    customerId: {
+        type: String,
+        label: "អតិថិជន",
+        optional: true,
+        autoform:{
+          firstOption: 'All'
+        }
+    },
+    fromDate: {
+        type: String,
+        label: "កាលបរិច្ឆេទចាប់ផ្ដើម",
+        optional:true
+    },
+    toDate: {
+        type: String,
+        label: "កាលបរិច្ឆេទបញ្ចប់",
+        optional:true
+
+    },
+    status: {
+        type: String,
+        label: "ប្រភេទវិក័យប័ត្រ",
+        optional: true,
+        autoform:{
+          firstOption: 'All'
+        }
+    },
+    staffId:{
+        type:String,
+        label:"បុគ្គលិក",
+        optional: true,
+        autoform:{
+          firstOption: 'All'
+        }
+    }
+});
