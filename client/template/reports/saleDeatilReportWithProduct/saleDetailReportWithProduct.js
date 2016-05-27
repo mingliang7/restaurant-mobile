170091127dbe2cbe78b0001486c0b147fe02bcb7
kevin <faithful.7up@gmail.com>
2016-05-27 09:10:07 +0700

    highlighted payment and partial invoice

--- client/template/reports/saleDeatilReportWithProduct/saleDetailReportWithProduct.js
+++ client/template/reports/saleDeatilReportWithProduct/saleDetailReportWithProduct.js
@@ -1,50 +1,60 @@
 Template.restaurantSaleDetailProductReport.onRendered(function() {
-  $('[name="fromDate"]').datetimepicker();
-  $('[name="toDate"]').datetimepicker();
+    $('[name="fromDate"]').datetimepicker();
+    $('[name="toDate"]').datetimepicker();
 });
 Template.restaurantSaleDetailProductReport.helpers({
-  customers() {
-    return ReactiveMethod.call('getCustomerList');
-  },
-  status() {
-    return [{
-      value: "active",
-      label: "កំពុងលក់"
-    }, {
-      value: "closed",
-      label: "ទូរទាត់ហើយ"
-    }, {
-      value: "partial",
-      label: "ជំពាក់"
-    }, {
-      value: "canceled",
-      label: "បោះបង់"
-    }];
-  },
-  users() {
-    return ReactiveMethod.call('getUserList');
-  }
+    customers() {
+        return ReactiveMethod.call('getCustomerList');
+    },
+    status() {
+        return [{
+            value: "active",
+            label: "កំពុងលក់"
+        }, {
+            value: "closed",
+            label: "ទូរទាត់ហើយ"
+        }, {
+            value: "partial",
+            label: "ជំពាក់"
+        }, {
+            value: "canceled",
+            label: "បោះបង់"
+        }, {
+            value: "transfer",
+            label: "ផ្ទេរ"
+        }];
+    },
+    users() {
+        return ReactiveMethod.call('getUserList');
+    }
 });
 
 Template.restaurantSaleDetailProductReportGen.helpers({
-  data: function() {
-    var query = Router.current().params.query;
-    var params = "saleReportWithProduct";
-    Fetcher.setDefault(params, false);
-    Fetcher.retrieve(params, 'getSaleReportWithProduct', query);
-    return Fetcher.get(params);
-  },
-  convertToDollar(amount){
-    let exchange = Restaurant.Collection.ExchangeRates.findOne({}, {sort: {created: -1}});
-    return numeral(amount * exchange.rates[0].rate).format('0,0');
-  },
-  statusCanceled(status){
-    if(status == 'canceled'){
-      return true;
+    data: function() {
+        var query = Router.current().params.query;
+        var params = "saleReportWithProduct";
+        Fetcher.setDefault(params, false);
+        Fetcher.retrieve(params, 'getSaleReportWithProduct', query);
+        return Fetcher.get(params);
+    },
+    convertToDollar(amount) {
+        let exchange = Restaurant.Collection.ExchangeRates.findOne({}, {
+            sort: {
+                created: -1
+            }
+        });
+        return numeral(amount / exchange.rates[0].rate).format('0,0');
+    },
+    statusCanceled(status) {
+        if (status == 'canceled' || 'transfer') {
+            return true;
+        }
+    },
+    partial(status) {
+        return status == 'partial'
     }
-  }
 });
 
 AutoForm.hooks({
-  restaurantSaleReport: {}
+    restaurantSaleReport: {}
 });
